8a1dd8574e869cf8344cc9de325b02de8a947a7a
bcity <bcity@45730098-6432-0510-a106-c033c7f6614a>
2016-05-18 07:11:37 +0000

    index

---
+++ final/src/main/java/su/controller/IndexController.java
@@ -0,0 +1,14 @@
+package su.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController 
+{
+	@RequestMapping("/index.do")
+	public String viewIndex()
+	{
+		return "index";
+	}
+}
