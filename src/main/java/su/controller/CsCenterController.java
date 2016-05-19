0a90108516b3f151f20334aa387469c93820cd3e
tnals1919 <tnals1919@45730098-6432-0510-a106-c033c7f6614a>
2016-05-19 11:17:08 +0000

    수

--- src/main/java/su/controller/CsCenterController.java
+++ src/main/java/su/controller/CsCenterController.java
@@ -1,30 +1,91 @@
 package su.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
+import su.csCenter.model.*;
 
 @Controller
 public class CsCenterController {
-
-	@RequestMapping("/csCenter.do")
-	public String csCenter(){
-		return "csCenter/notice";
+	@Autowired
+	private CsNoticeDAO csnoticeDao;
+	private CsoneandoneDAO csoneandoneDao;
+	
+	public CsoneandoneDAO getCsoneandoneDao() {
+		return csoneandoneDao;
 	}
-	@RequestMapping("/notice.do")
-		public String notice(){
-			return "csCenter/notice";
-		}
+
+	public void setCsoneandoneDao(CsoneandoneDAO csoneandoneDao) {
+		this.csoneandoneDao = csoneandoneDao;
+	}
+
+	public CsNoticeDAO getCsnoticeDao() {
+		return csnoticeDao;
+	}
+
+	public void setCsnoticeDao(CsNoticeDAO csnoticeDao) {
+		this.csnoticeDao = csnoticeDao;
+	}
+
+	//@RequestMapping("/csCenter.do")
+	//public String csCenter(){
+		//return "csCenter/notice";
+	//}
+	//공지사항 리스트
+	@RequestMapping("/csCenter.do")
+		public ModelAndView noticeList(@RequestParam(value="cp",defaultValue="1")int cp){
+		int totalCnt=csnoticeDao.noticeTotalCnt();
+		int listSize=10;
+		int pageSize=5;
+		List<CsNoticeDTO> list=csnoticeDao.noticeList(cp,listSize);
+		String pageStr=
+			su.Page.SuPage.makePage("csCenter.do", totalCnt, listSize, pageSize, cp);
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("list", list);
+		mav.addObject("pageStr",pageStr);
+		mav.setViewName("csCenter/notice");
+		return mav;
+	}
+	//공지사항 글보기
+	@RequestMapping("/noticeContent.do")
+	public ModelAndView noticeContent(int idx){
+		int result =csnoticeDao.noticeReadnum(idx);
+		List<CsNoticeDTO> list=csnoticeDao.noticeContent(idx);
+		
+ 		ModelAndView mav=new ModelAndView();
+ 		mav.addObject("result",result);
+		mav.addObject("list", list);
+		mav.setViewName("csCenter/noticeContent");
+		return mav;
+	}
+		
 	
 	@RequestMapping("/qnaList.do")
 		public String qndList(){
 			return "csCenter/qnaList";
 		}
+	//1:1문의 리스트
 	@RequestMapping("/oneAndOne.do")
-	public String oneAndOne(){
-		return "csCenter/oneAndOne";
+	public ModelAndView oneandoneList(@RequestParam(value="cp",defaultValue="1")int cp){
+		int totalCnt=csoneandoneDao.oneandoneTotalCnt();
+		int listSize=10;
+		int pageSize=5;
+		List<CsoneandoneDTO> list=csoneandoneDao.oneandoneList(cp,listSize);
+		String pageStr=
+			su.Page.SuPage.makePage("oneAndOne.do", totalCnt, listSize, pageSize, cp);
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("list", list);
+		mav.addObject("pageStr",pageStr);
+		mav.setViewName("csCenter/oneAndOne");
+		return mav;
 	}
+
+	
 	@RequestMapping("useGuideList.do")
 	public String useGuideList(){
 		return "csCenter/useGuideList";
