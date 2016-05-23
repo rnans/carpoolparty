b9df8b5ba2fc4ecb036fee1ee5e7d350f0b19f25
tnals1919 <tnals1919@45730098-6432-0510-a106-c033c7f6614a>
2016-05-23 01:50:52 +0000

    ㅎㅎ

---
+++ final/src/main/java/su/admin/controller/AdimCsCenterController.java
@@ -0,0 +1,40 @@
+package su.admin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import su.csCenter.model.*;
+@Controller
+public class AdimCsCenterController {
+
+	@Autowired
+	private CsNoticeDAO csnoticeDao;
+
+	public CsNoticeDAO getCsnoticeDao() {
+		return csnoticeDao;
+	}
+
+	public void setCsnoticeDao(CsNoticeDAO csnoticeDao) {
+		this.csnoticeDao = csnoticeDao;
+	}
+	@RequestMapping("/adminNoticeList.do")
+	public ModelAndView noticeList(@RequestParam(value="cp",defaultValue="1")int cp){
+		int totalCnt=csnoticeDao.noticeTotalCnt();
+		int listSize=10;
+		int pageSize=5;
+		List<CsNoticeDTO> list=csnoticeDao.noticeList(cp,listSize);
+		String pageStr=
+			su.Page.SuPage.makePage("adminNoticeList.do", totalCnt, listSize, pageSize, cp);
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("list", list);
+		mav.addObject("pageStr",pageStr);
+		mav.setViewName("admin/noticeList");
+		return mav;
+	}
+	}
+
