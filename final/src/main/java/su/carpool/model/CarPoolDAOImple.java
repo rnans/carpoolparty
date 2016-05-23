8a5fc3a8f126677919353c08be86ca3e141a94e7
kjn90901 <kjn90901@45730098-6432-0510-a106-c033c7f6614a>
2016-05-23 04:00:51 +0000

    박양선

---
+++ final/src/main/java/su/carpool/model/CarPoolDAOImple.java
@@ -0,0 +1,32 @@
+package su.carpool.model;
+
+import java.util.List;
+import java.util.*;
+import su.member.model.*;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+public class CarPoolDAOImple implements CarPoolDAO {
+
+	private SqlSessionTemplate sqlMap;
+
+	// 생성자 방식
+	public CarPoolDAOImple(SqlSessionTemplate sqlMap) {
+		super();
+		this.sqlMap = sqlMap;
+	}
+	public CarPoolDTO get
+
+	public MemberDTO getUserInfo(String userid) {
+
+		List<MemberDTO> list = sqlMap.selectList("getUserInfo", userid);
+
+		if (list == null) {
+			return null;
+		}
+
+		MemberDTO dto = (MemberDTO) list.get(0);
+
+		return dto;
+	}
+}
