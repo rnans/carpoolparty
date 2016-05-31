6fdf66c2a3d0364cf8dc047774a481afaeb5eaaa
tnals1919 <tnals1919@45730098-6432-0510-a106-c033c7f6614a>
2016-05-31 10:21:01 +0000

    fas

--- src/main/java/su/controller/MessageController.java
+++ src/main/java/su/controller/MessageController.java
@@ -1,22 +1,112 @@
 package su.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
+
+import su.message.model.*;
 
 @Controller
 public class MessageController {
+	@Autowired
+	public MessageDAO messageDao;
 
 	
-	 @RequestMapping("/messageSend.do")
+	 public MessageDAO getMessageDao() {
+		return messageDao;
+	}
+	public void setMessageDao(MessageDAO messageDao) {
+		this.messageDao = messageDao;
+	}
+	@RequestMapping("/messageSend.do")
 	 public ModelAndView messageSend(HttpServletRequest req){
 		 String id = req.getParameter("userid");
 		 ModelAndView mav=new ModelAndView();
 		 mav.addObject("id",id);
 		 mav.setViewName("message/messageSend");
 		 return mav;
-		
 	 }
-}
+	 @RequestMapping("/messageSend_ok.do")
+	 public ModelAndView messageSend_ok(MessageDTO dto){
+		 int result=messageDao.messageSend(dto);
+		 String msg=result>0?"쪽지보내기성공":"쪽지보내기실패";
+		 ModelAndView mav=new ModelAndView();
+		 mav.addObject("msg", msg);
+		 mav.setViewName("message/messageMsg");
+		 return mav;
+	 }
+	 @RequestMapping("/messageShow.do")
+	 public ModelAndView messageShow(@RequestParam(value="cp",defaultValue="1")int cp,HttpSession session){
+	
+		 	String sid=(String)session.getAttribute("sid");
+			int totalCnt=messageDao.messageTotalCnt();
+			int listSize=10;
+			int pageSize=5;
+			List<MessageDTO> list=messageDao.messageShow(cp,listSize,sid);
+			String pageStr=
+				su.Page.SuPage.makePage("messageShow.do", totalCnt, listSize, pageSize, cp);
+			ModelAndView mav=new ModelAndView();
+			mav.addObject("list", list);
+			mav.addObject("pageStr",pageStr);
+			mav.setViewName("message/test1");
+			return mav;
+		}
+	 @RequestMapping("/messageContent.do")
+	 public ModelAndView messageContent(HttpServletRequest req){
+		 int idx=Integer.parseInt(req.getParameter("idx"));
+		 List<MessageDTO> list=messageDao.messageContent(idx);
+		 int result=messageDao.messageReading(idx);
+		 ModelAndView mav=new ModelAndView();
+		 mav.addObject("list", list);
+		 mav.addObject("result", result);
+		 mav.setViewName("message/test2");
+		 return mav;
+	 }
+	 @RequestMapping("/messageRewrite.do")
+	 public ModelAndView messageRewrite(HttpServletRequest req){
+	      	 String sendid=req.getParameter("sendid");
+	      	 ModelAndView mav=new ModelAndView();
+	      	 mav.addObject("receiveid", sendid);
+	         mav.setViewName("message/test3");
+	         return mav;
+	 }
+	 
+	 @RequestMapping("/messageRewrite_ok.do")
+	 public ModelAndView messageRewrite_ok(MessageDTO dto){
+		 int result=messageDao.messageRewrite(dto);
+		 String msg=result>0?"답장성공!":"답장실패!";
+		 ModelAndView mav=new ModelAndView();
+		 mav.addObject("msg", msg);
+		 mav.setViewName("message/messageMsg");
+		 return mav;
+		 
+	 }
+	@RequestMapping("/messageDel.do")
+	public ModelAndView messageDel(HttpServletRequest req){
+		int idx=Integer.parseInt(req.getParameter("idx"));
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("idx", idx);
+		mav.setViewName("message/messageDel");
+		return mav;
+	}
+	@RequestMapping("/messageDel_ok.do")
+	public ModelAndView messageDel_ok(int idx){
+		int result=messageDao.messageDel(idx);
+		String msg=result>0?"쪽지삭제성공!":"쪽지삭제실패!";
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg", msg);
+		mav.setViewName("message/messageMsg");
+		return mav;
+	}
+		 
+	 }
+
